47286c3e0674bccc39b8d7a334b775cbf10dbfbe
Muhammad Rezvin <57952532+madrez19@users.noreply.github.com>
2024-03-01 04:09:07 +0700

    Delete belajar-npm-tea-test directory

--- belajar-npm-tea-test/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.stripTags = function(html = "") {
-  // <b> or </b> => "" ""
-  if(typeof html !== "string") return html
-  const text = html.replace(/<[^>]+>/gi, "")
-  return text
-}
